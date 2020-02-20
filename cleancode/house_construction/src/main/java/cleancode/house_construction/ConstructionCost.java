4d66f2a1f63f72b8dc3a2225e0a81eafc51d6f51
ARE SAI PRASANNA LAKSHMI <aresaiprasanna1999@gmail.com>
2020-02-20 10:12:46 +0530

    clean code

--- cleancode/house_construction/src/main/java/cleancode/house_construction/ConstructionCost.java
+++ cleancode/house_construction/src/main/java/cleancode/house_construction/ConstructionCost.java
@@ -4,13 +4,13 @@ public class ConstructionCost
 {
 	public float construction_cost(int n ,int automatecost,float sqfeet)
 	{
-		return (n*sqfeet+automatecost);
+		return (n+automatecost)*sqfeet;
 	}
 	public int automated(int flag1)
 	{
 		
 		int cost;
-		if(flag1>1)
+		if(flag1>0)
 		{
 			 cost=700;
 		}
